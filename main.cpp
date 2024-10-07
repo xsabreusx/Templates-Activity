0efcf21531296db92459bee50448796c1ee4cbf8
Humbert <122186863+xsabreusx@users.noreply.github.com>
2024-10-07 16:26:44 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
+
 #include <iostream>
+using namespace std;
+
+/*
+Write a function template named max_two that finds the largest value among two numbers.
+Write a function template named max_three that finds the largest value among three numbers.
+Write a function template named swap that exchanges the values of two parameters.
+*/
 
 template <typename T>
 T max_two (T M, T N) {
@@ -7,12 +15,27 @@ T max_two (T M, T N) {
     if (N > M) return N;
 }
 
+template<typename T, typename U, typename V>
+T max_three(T a, U b, V c){
+    if (a > b && a > c)
+    {
+        return a;
+    }
+    else if ( b > a && b > c)
+    {
+        return b;
+    }
+    else if ( c > a && c > b)
+    {
+        return c;
+    }
+    else return 0;
+}
 
 
 
-int main () {
+int main(){
 
     std::cout << max_two (5.6, 6.7);
 
 }
-
